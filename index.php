7070f8ef08c3c64d134641b84ba4198c9ed783d4
Nico Arato <nico.arato@gmail.com>
2019-11-22 19:31:15 -0300

    Estilos del formulario.

--- index.php
+++ index.php
@@ -15,16 +15,16 @@
             </div>
             <div class="campo">    
                 <label for="empresa">Empresa</label>
-                <input type="text" placeholder="Nombre de contacto" id="empresa">
+                <input type="text" placeholder="Nombre de la empresa" id="empresa">
             </div>
             <div class="campo">
                 <label for="telefono">Teléfono</label>
                 <input type="tel" placeholder="Teléfono" id="telefono">
             </div>
+        </div>
             <div class="campo enviar">
                 <input type="submit" value="Añadir">
             </div>
-        </div>
     </form>
 </div>
  
