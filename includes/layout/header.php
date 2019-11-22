ae952159f0019202133a456cfadcdfaff69f17b9
Nico Arato <nico.arato@gmail.com>
2019-11-22 20:32:06 -0300

    Listado de contactos.

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet"> <!--load all styles -->
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -27,6 +27,76 @@
             </div>
     </form>
 </div>
+
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+        <h2>Contactos</h2>
+
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar contactos...">
+
+        <p class="total-contactos">
+            <span>2</span>
+            Contactos
+        </p>
+
+        <div class="contenedor-tabla">
+            <table id="listado-contactos" class="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Teléfono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nicolas</td>
+                        <td>Ripcurl</td>
+                        <td>1235456</td>
+                        <td>
+                            <a href="#" class="btn-editar btn">
+                                <i class="fas fa-user-edit"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>Leonardo</td>
+                        <td>Ripcurl</td>
+                        <td>1235456</td>
+                        <td>
+                            <a href="#" class="btn-editar btn">
+                                <i class="fas fa-user-edit"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>PEdrito</td>
+                        <td>Ripcurl</td>
+                        <td>1235456</td>
+                        <td>
+                            <a href="#" class="btn-editar btn">
+                                <i class="fas fa-user-edit"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </td>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
  
 
 
