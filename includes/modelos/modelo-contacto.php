5471b1e6b966e45d5b48f2dfb3277c173cdf12f7
Nico Arato <nico.arato@gmail.com>
2019-11-23 02:50:16 -0300

    Insertando registros en la base de datos.

--- includes/modelos/modelo-contacto.php
+++ includes/modelos/modelo-contacto.php
@@ -1,3 +1,43 @@
 <?php 
 
-echo json_encode($_POST);
+if($_POST['accion'] == 'crear'){
+
+    //creará un nuevo registro en la base de datos
+
+    require_once('../funciones/bd.php');
+
+    //validar las entradas
+    $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+    $empresa = filter_var($_POST['empresa'], FILTER_SANITIZE_STRING);
+    $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+
+    try {
+        //statement
+        $stmt = $conn->prepare("INSERT INTO contactos (nombre, empresa, telefono) VALUES (?,?,?)");
+        $stmt->bind_param("sss", $nombre, $empresa, $telefono); //evita injection sql
+        $stmt->execute();
+        if($stmt->affected_rows == 1){
+            $respuesta = array(
+                'respuesta' => 'correcto',
+                'datos' => array(
+                    'nombre' => $nombre,
+                    'empresa' => $empresa,
+                    'telefono' => $telefono,
+                    'id_insertado' => $stmt->insert_id
+                )
+            );
+        }
+        $stmt->close();
+        $conn->close();
+
+    }catch(Exception $e) {
+        $respuesta = array(
+            'error' => $e->getMessage()
+        );
+    }
+
+
+
+    echo json_encode($respuesta);
+
+}
