391b77c4bab791de6496b87c0e40f2bc7ddd794d
Nico Arato <nico.arato@gmail.com>
2020-04-17 03:35:26 -0300

    Borrar contacto de base de datos y de html.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
 const formularioContactos = document.querySelector('#contacto'),
-        listadoContactos = document.querySelector('#listado-contactos tbody');
+    listadoContactos = document.querySelector('#listado-contactos tbody');
 
 eventListeners();
 
-function eventListeners(){
+function eventListeners() {
     //cuando el formulario se ejecuta
     formularioContactos.addEventListener('submit', leerFormulario);
+
+    //Listener para eliminar el boton
+    listadoContactos.addEventListener('click', eliminarContacto);
 }
 
-function leerFormulario(e){
+function leerFormulario(e) {
     e.preventDefault(e); //recomendable cuando usamos js o ajax-- previene accion por defecto
- 
-    //leer los datos de los inputs
-    const   nombre = document.querySelector('#nombre').value,
-            empresa = document.querySelector('#empresa').value,
-            telefono = document.querySelector('#telefono').value,
-            accion = document.querySelector('#accion').value;
 
-    if(nombre === '' || empresa === '' || telefono === '' ){
+    //leer los datos de los inputs
+    const nombre = document.querySelector('#nombre').value,
+        empresa = document.querySelector('#empresa').value,
+        telefono = document.querySelector('#telefono').value,
+        accion = document.querySelector('#accion').value;
+
+    if (nombre === '' || empresa === '' || telefono === '') {
         //2 parametros texto y clases
-        mostrarNotificacion('Todos los campos son obligatorios', 'error' );
+        mostrarNotificacion('Todos los campos son obligatorios', 'error');
         //mostrarNotificacion('Contacto correcto', 'error' );
-    }else {
+    } else {
         //pasa la validacion crear llamado a Ajax
         const infoContacto = new FormData();
         infoContacto.append('nombre', nombre);
@@ -30,10 +33,10 @@ function leerFormulario(e){
         infoContacto.append('accion', accion);
 
         //console.log(...infoContacto);
-        if(accion === 'crear'){
+        if (accion === 'crear') {
             //crear nuevo contacto
             insertarBD(infoContacto);
-        }else{
+        } else {
             //editar contacto
 
         }
@@ -41,18 +44,18 @@ function leerFormulario(e){
 }
 
 /*inserta en la bdatos via ajax */
-function insertarBD(datos){
+function insertarBD(datos) {
     //llamado a ajax
-    
+
     //crear el objeto
-    const xhr = new XMLHttpRequest(); 
+    const xhr = new XMLHttpRequest();
 
     //abrir la conexion
-    xhr.open('POST', 'includes/modelos/modelo-contacto.php', true );
+    xhr.open('POST', 'includes/modelos/modelo-contacto.php', true);
 
     //pasar los datos
-    xhr.onload = function(){
-        if(this.status === 200){
+    xhr.onload = function() {
+        if (this.status === 200) {
             console.log(JSON.parse(xhr.responseText));
 
             //leemos la respuesta de php
@@ -61,7 +64,7 @@ function insertarBD(datos){
             //inserta un nuevo elemento a la tabla
             const nuevoContacto = document.createElement('tr');
 
-            nuevoContacto.innerHTML= `
+            nuevoContacto.innerHTML = `
                 <td>${respuesta.datos.nombre}</td>
                 <td>${respuesta.datos.empresa}</td>
                 <td>${respuesta.datos.telefono}</td>
@@ -115,8 +118,50 @@ function insertarBD(datos){
     xhr.send(datos);
 }
 
+//Eliminar el contacto
+function eliminarContacto(e) {
+    if (e.target.parentElement.classList.contains('btn-borrar')) {
+        //tomar el id
+        const id = e.target.parentElement.getAttribute('data-id');
+
+        //preguntar al usuario
+        const respuesta = confirm('¿Estás seguro?');
+
+        if (respuesta) {
+
+            //llamado a ajax
+            const xhr = new XMLHttpRequest();
+            // crear conexxiion
+            xhr.open('GET', `includes/modelos/modelo-contacto.php?id=${id}&accion=borrar`, true);
+            //leer respuesta
+            xhr.onload = function() {
+                    if (this.status === 200) {
+                        const resultado = JSON.parse(xhr.responseText);
+
+                        if (resultado.respuesta == 'correcto') {
+                            //eliminar registro del dom
+                            console.log(e.target.parentElement.parentElement.parentElement);
+                            e.target.parentElement.parentElement.parentElement.remove();
+
+                            //mostrar notificacion
+                            mostrarNotificacion('Contacto eliminado.', 'correcto');
+                        } else {
+                            //mostramos una notificacion
+                            mostrarNotificacion('Hubo un error...', 'error');
+                        }
+
+                    }
+                }
+                //enviar la peticion
+            xhr.send();
+        }
+    }
+}
+
+
+
 //notificiacion en pantalla.
-function mostrarNotificacion(mensaje, clase){
+function mostrarNotificacion(mensaje, clase) {
     const notificacion = document.createElement('div');
     notificacion.classList.add(clase, 'notificacion', 'sombra');
     notificacion.textContent = mensaje;
@@ -128,10 +173,10 @@ function mostrarNotificacion(mensaje, clase){
 
     setTimeout(() => {
         notificacion.classList.add('visible');
-        setTimeout( () => {
+        setTimeout(() => {
             notificacion.classList.remove('visible');
-            setTimeout( () => {
-                notificacion.remove();               
+            setTimeout(() => {
+                notificacion.remove();
             }, 500)
 
         }, 3000);
